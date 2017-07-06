91db3537f1390ac6d6acccae1e0ee6ee9164bb0c
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-06 16:36:43 +0100

    Initial import of unr into userspace.

--- libdtrace-core/unr_shim.h
+++ libdtrace-core/unr_shim.h
@@ -1,8 +1,21 @@
 #ifndef _UNR_SHIM_H_
-#define _UNR_SHIM_H_
+#define	_UNR_SHIM_H_
+
+#include <sys/queue.h>
+#include <pthread.h>
 
 struct unrhdr {
-	int cnt;
+	TAILQ_HEAD(unrhd,unr)	head;
+	u_int			low;	/* Lowest item */
+	u_int			high;	/* Highest item */
+	u_int			busy;	/* Count of allocated items */
+	u_int			alloc;	/* Count of memory allocations */
+	u_int			first;	/* items in allocated from start */
+	u_int			last;	/* items free at end */
+	pthread_mutex_t		*mtx;
+	TAILQ_HEAD(unrfr,unr)	ppfree;	/* Items to be freed after mtx
+					   lock dropped */
 };
 
-#endif /* _UNR_SHIM_H_ */
+#endif
+
