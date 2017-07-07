781ab8d89dc485681f4b185758aa418725106313
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-07 10:49:37 +0100

    Convert all of the libcheck test-cases into ATF-based test-cases.

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#include <check.h>
+#include <atf-c.h>
 
 #include "../libdtrace-core/dtrace.h"
 #include "../libdtrace-core/dtrace_impl.h"
@@ -29,7 +29,8 @@ static dtrace_pattr_t test_provider_attr = {
 { DTRACE_STABILITY_STABLE, DTRACE_STABILITY_STABLE, DTRACE_CLASS_COMMON },
 };
 
-START_TEST(test_dtrace_init)
+ATF_TC_WITHOUT_HEAD(dtrace_init);
+ATF_TC_BODY(dtrace_init, tc)
 {
 	/*
 	 * Tests the initialization of the library
@@ -37,188 +38,150 @@ START_TEST(test_dtrace_init)
 	int err;
 
 	err = dtrace_init();
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 }
-END_TEST
 
-START_TEST(test_dtrace_deinit)
+ATF_TC_WITHOUT_HEAD(dtrace_deinit);
+ATF_TC_BODY(dtrace_deinit, tc)
 {
-	int err;
 	/*
 	 * Initializes the library and tests it's de-initialization
 	 */
+	int err;
 
 	err = dtrace_init();
 	if (err != 0)
-		ck_abort_msg("DTrace not properly initialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly initialized: %s", strerror(err));
 
 	err = dtrace_deinit();
 
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 }
-END_TEST
 
-START_TEST(test_dtrace_providers)
+ATF_TC_WITHOUT_HEAD(dtrace_providers);
+ATF_TC_BODY(dtrace_providers, tc)
 {
-	char *provs;
-	size_t sz;
-	int err;
 	/*
 	 * Test the dtrace_providers() function
 	 */
+	char *provs;
+	size_t sz;
+	int err;
 
 	err = dtrace_init();
 	if (err != 0)
-		ck_abort_msg("DTrace not properly initialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly initialized: %s", strerror(err));
 
 	provs = dtrace_providers(&sz);
 
-	ck_assert_int_eq(sz, 1);
-	ck_assert_str_eq(provs, "dtrace");
+	ATF_CHECK_EQ(1, sz);
+	ATF_CHECK_STREQ("dtrace", provs);
 
 	err = dtrace_deinit();
 	if (err != 0)
-		ck_abort_msg("DTrace not properly deinitialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly deinitialized: %s", strerror(err));
 }
-END_TEST
 
-START_TEST(test_dtrace_register)
+ATF_TC_WITHOUT_HEAD(dtrace_register);
+ATF_TC_BODY(dtrace_register, tc)
 {
+	/*
+	 * Test the provider registration
+	 */
 	dtrace_provider_id_t id;
 	int err;
 	size_t sz;
 	char (*provs)[DTRACE_PROVNAMELEN];
 
-	/*
-	 * Test the provider registration
-	 */
-
 	err = dtrace_init();
 	if (err)
-		ck_abort_msg("DTrace not properly initialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly initialized: %s", strerror(err));
 
 	err = dtrace_register("test_provider", &test_provider_attr,
 	    DTRACE_PRIV_NONE, 0, &test_provider_ops, NULL, &id);
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 
 	provs = (char (*)[DTRACE_PROVNAMELEN]) dtrace_providers(&sz);
-	ck_assert_int_eq(sz, 2);
-	ck_assert_str_eq("dtrace", provs[0]);
-	ck_assert_str_eq("test_provider", provs[1]);
+	ATF_CHECK_EQ(2, sz);
+	ATF_CHECK_STREQ(provs[0], "dtrace");
+	ATF_CHECK_STREQ(provs[1], "test_provider");
 
 	err = dtrace_unregister(id);
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 
 	err = dtrace_deinit();
 	if (err)
-		ck_abort_msg("DTrace not properly deinitialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly deinitialized: %s", strerror(err));
 }
-END_TEST
 
-START_TEST(test_dtrace_probe_create)
+ATF_TC_WITHOUT_HEAD(dtrace_probe_create);
+ATF_TC_BODY(dtrace_probe_create, tc)
 {
-	dtrace_id_t probeid;
-	dtrace_provider_id_t id;
-	int err;
 	/*
 	 * Test probe creation
 	 */
+	dtrace_id_t probeid;
+	dtrace_provider_id_t id;
+	int err;
 
 	err = dtrace_init();
 	if (err)
-		ck_abort_msg("DTrace not properly initialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly initialized: %s", strerror(err));
 
 	err = dtrace_register("test_provider", &test_provider_attr,
 	    DTRACE_PRIV_NONE, 0, &test_provider_ops, NULL, &id);
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 
 	probeid = dtrace_probe_create(id, "test", "probe",
 	    "foo", 0, NULL);
 
 	err = dtrace_unregister(id);
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 
 	err = dtrace_deinit();
 	if (err)
-		ck_abort_msg("DTrace not properly deinitialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly deinitialized: %s", strerror(err));
 }
-END_TEST
 
-/*
- * TODO: Should make some probe_lookup() tests here in order to ensure that they
- * are correct, so that we can use them in tests with respect to probe
- * creation/deletion...
- */
-
-START_TEST(test_dtrace_probe_lookup)
+ATF_TC_WITHOUT_HEAD(dtrace_probe_lookup);
+ATF_TC_BODY(dtrace_probe_lookup, tc)
 {
-	dtrace_id_t probeid, lookupid;
-	dtrace_provider_id_t id;
-	int err;
 	/*
 	 * Test probe creation
 	 */
+	dtrace_id_t probeid, lookupid;
+	dtrace_provider_id_t id;
+	int err;
 
 	err = dtrace_init();
 	if (err)
-		ck_abort_msg("DTrace not properly initialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly initialized: %s", strerror(err));
 
 	err = dtrace_register("test_provider", &test_provider_attr,
 	    DTRACE_PRIV_NONE, 0, &test_provider_ops, NULL, &id);
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 
 	probeid = dtrace_probe_create(id, "test", "probe",
 	    "foo", 0, NULL);
 	lookupid = dtrace_probe_lookup(id, "test", "probe", "foo");
-	ck_assert_int_eq(probeid, lookupid);
+	ATF_CHECK_EQ(probeid, lookupid);
 
 	err = dtrace_unregister(id);
-	ck_assert_int_eq(err, 0);
+	ATF_CHECK_EQ(0, err);
 
 	err = dtrace_deinit();
 	if (err)
-		ck_abort_msg("DTrace not properly deinitialized: %s", strerror(err));
+		atf_tc_fail("DTrace not properly deinitialized: %s", strerror(err));
 }
-END_TEST
 
-static Suite *
-create_dtrace_suite(void)
+ATF_TP_ADD_TCS(tp)
 {
-	Suite *s;
-	TCase *tc_core;
+	ATF_TP_ADD_TC(tp, dtrace_init);
+	ATF_TP_ADD_TC(tp, dtrace_deinit);
+	ATF_TP_ADD_TC(tp, dtrace_providers);
+	ATF_TP_ADD_TC(tp, dtrace_register);
+	ATF_TP_ADD_TC(tp, dtrace_probe_create);
+	ATF_TP_ADD_TC(tp, dtrace_probe_lookup);
 
-	s = suite_create("DTrace test suite");
-
-	tc_core = tcase_create("Core");
-
-	tcase_add_test(tc_core, test_dtrace_init);
-	tcase_add_test(tc_core, test_dtrace_deinit);
-	tcase_add_test(tc_core, test_dtrace_providers);
-	tcase_add_test(tc_core, test_dtrace_register);
-	tcase_add_test(tc_core, test_dtrace_probe_create);
-	tcase_add_test(tc_core, test_dtrace_probe_lookup);
-
-	suite_add_tcase(s, tc_core);
-
-	return (s);
+	return (atf_no_error());
 }
-
-int
-main(void)
-{
-	Suite *dt_suite;
-	SRunner *dt_sr;
-	int n_failed;
-
-	dt_suite = create_dtrace_suite();
-	dt_sr = srunner_create(dt_suite);
-
-	srunner_set_tap(dt_sr, "test-output");
-	srunner_run_all(dt_sr, CK_VERBOSE);
-	n_failed = srunner_ntests_failed(dt_sr);
-
-	srunner_free(dt_sr);
-
-	return ((n_failed > 0) ? 1 : 0);
-}
-
