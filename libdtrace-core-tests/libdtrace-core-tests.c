40aa11df16c2ad862808e400d30435c2542d8945
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-11 17:00:55 +0100

    Test both positive and negative numbers with BG.

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -1063,11 +1063,83 @@ ATF_TC_BODY(DIF_OP_BNE, tc)
 	free(estate);
 }
 
-ATF_TC_WITHOUT_HEAD(DIF_OP_BG_SUCCESS);
-ATF_TC_BODY(DIF_OP_BG_SUCCESS, tc)
+ATF_TC_WITHOUT_HEAD(DIF_OP_BG_SUCCESS_POS);
+ATF_TC_BODY(DIF_OP_BG_SUCCESS_POS, tc)
 {
 	/*
 	 * Test the BG operation of the DTrace machine when it branches.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_z = 0;
+	estate->dtes_cc_n = 0;
+	estate->dtes_cc_v = 0;
+
+	instr = DIF_INSTR_BRANCH(DIF_OP_BG, 0xD06E);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0xD06E, estate->dtes_pc);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
+ATF_TC_WITHOUT_HEAD(DIF_OP_BG_FAIL_POS);
+ATF_TC_BODY(DIF_OP_BG_FAIL_POS, tc)
+{
+	/*
+	 * Test the BG operation of the DTrace machine when it doesn't branch.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_z = 0;
+	estate->dtes_cc_n = 0;
+	estate->dtes_cc_v = 1;
+
+	instr = DIF_INSTR_BRANCH(DIF_OP_BG, 0xD06E);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0, estate->dtes_pc);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
+ATF_TC_WITHOUT_HEAD(DIF_OP_BG_SUCCESS_NEG);
+ATF_TC_BODY(DIF_OP_BG_SUCCESS_NEG, tc)
+{
+	/*
+	 * Test the BG operation of the DTrace machine when it doesn't branch.
 	 */
 	dtrace_mstate_t *mstate;
 	dtrace_vstate_t *vstate;
@@ -1099,8 +1171,8 @@ ATF_TC_BODY(DIF_OP_BG_SUCCESS, tc)
 	free(estate);
 }
 
-ATF_TC_WITHOUT_HEAD(DIF_OP_BG_FAIL);
-ATF_TC_BODY(DIF_OP_BG_FAIL, tc)
+ATF_TC_WITHOUT_HEAD(DIF_OP_BG_FAIL_NEG);
+ATF_TC_BODY(DIF_OP_BG_FAIL_NEG, tc)
 {
 	/*
 	 * Test the BG operation of the DTrace machine when it doesn't branch.
@@ -1169,8 +1241,10 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, DIF_OP_BA);
 	ATF_TP_ADD_TC(tp, DIF_OP_BE);
 	ATF_TP_ADD_TC(tp, DIF_OP_BNE);
-	ATF_TP_ADD_TC(tp, DIF_OP_BG_SUCCESS);
-	ATF_TP_ADD_TC(tp, DIF_OP_BG_FAIL);
+	ATF_TP_ADD_TC(tp, DIF_OP_BG_SUCCESS_POS);
+	ATF_TP_ADD_TC(tp, DIF_OP_BG_FAIL_POS);
+	ATF_TP_ADD_TC(tp, DIF_OP_BG_SUCCESS_NEG);
+	ATF_TP_ADD_TC(tp, DIF_OP_BG_FAIL_NEG);
 #endif
 
 	return (atf_no_error());
