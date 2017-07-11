be2fce4f07e2a7b645c36a27fb71f28aa72e0025
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-11 16:49:04 +0100

    Implement tests for DIF_OP_BE and DIF_OP_BNE.

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -973,6 +973,92 @@ ATF_TC_BODY(DIF_OP_BA, tc)
 	free(estate);
 }
 
+ATF_TC_WITHOUT_HEAD(DIF_OP_BE);
+ATF_TC_BODY(DIF_OP_BE, tc)
+{
+	/*
+	 * Test the BE operation of the DTrace machine.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_z = 0;
+
+	instr = DIF_INSTR_BRANCH(DIF_OP_BE, 0xD06E);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0, estate->dtes_pc);
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_z = 1;
+
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0xD06E, estate->dtes_pc);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
+ATF_TC_WITHOUT_HEAD(DIF_OP_BNE);
+ATF_TC_BODY(DIF_OP_BNE, tc)
+{
+	/*
+	 * Test the BNE operation of the DTrace machine.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_z = 0;
+
+	instr = DIF_INSTR_BRANCH(DIF_OP_BNE, 0xD06E);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0xD06E, estate->dtes_pc);
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_z = 1;
+
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0, estate->dtes_pc);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
 #endif
 
 ATF_TP_ADD_TCS(tp)
@@ -1005,6 +1091,8 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, DIF_OP_CMP_R1_LT_R2);
 	ATF_TP_ADD_TC(tp, DIF_OP_TST);
 	ATF_TP_ADD_TC(tp, DIF_OP_BA);
+	ATF_TP_ADD_TC(tp, DIF_OP_BE);
+	ATF_TP_ADD_TC(tp, DIF_OP_BNE);
 #endif
 
 	return (atf_no_error());
