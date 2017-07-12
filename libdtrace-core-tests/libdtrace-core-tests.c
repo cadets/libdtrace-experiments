dba9b764db3c5606364efdedc173c1f3b716d4f3
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-12 09:32:52 +0100

    Add tests covering the DIF_OP_BGE instruction.

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -1279,6 +1279,76 @@ ATF_TC_BODY(DIF_OP_BGU_FAIL, tc)
 	free(estate);
 }
 
+ATF_TC_WITHOUT_HEAD(DIF_OP_BGE_SUCCESS);
+ATF_TC_BODY(DIF_OP_BGE_SUCCESS, tc)
+{
+	/*
+	 * Test the BGE operation of the DTrace machine when it branches.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_n = 1;
+	estate->dtes_cc_v = 1;
+
+	instr = DIF_INSTR_BRANCH(DIF_OP_BGE, 0xD06E);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0xD06E, estate->dtes_pc);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
+ATF_TC_WITHOUT_HEAD(DIF_OP_BGE_FAIL);
+ATF_TC_BODY(DIF_OP_BGE_FAIL, tc)
+{
+	/*
+	 * Test the BGE operation of the DTrace machine when it doesn't branch.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_pc = 0;
+	estate->dtes_cc_n = 1;
+	estate->dtes_cc_v = 0;
+
+	instr = DIF_INSTR_BRANCH(DIF_OP_BGE, 0xD06E);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0, estate->dtes_pc);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
 #endif
 
 ATF_TP_ADD_TCS(tp)
@@ -1319,6 +1389,8 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, DIF_OP_BG_FAIL_NEG);
 	ATF_TP_ADD_TC(tp, DIF_OP_BGU_SUCCESS);
 	ATF_TP_ADD_TC(tp, DIF_OP_BGU_FAIL);
+	ATF_TP_ADD_TC(tp, DIF_OP_BGE_SUCCESS);
+	ATF_TP_ADD_TC(tp, DIF_OP_BGE_FAIL);
 #endif
 
 	return (atf_no_error());
