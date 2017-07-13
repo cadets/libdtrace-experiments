9da856834c3289f2f21b278f3b7b680096cc9491
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-13 13:37:33 +0100

    Implement a test which demonstrates expected behaviour of DIF_OP_SCMP when the string is too large for DTrace to compare with the given options.

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -2902,6 +2902,51 @@ ATF_TC_BODY(DIF_OP_SCMP_STR1_LT_STR2, tc)
 	free(estate);
 }
 
+ATF_TC_WITHOUT_HEAD(DIF_OP_SCMP_FAIL);
+ATF_TC_BODY(DIF_OP_SCMP_FAIL, tc)
+{
+	/*
+	 * Test the SCMP operation of the DTrace machine when the wrong size of
+	 * the string is specified. The expected behaviour here is that the
+	 * string will only be compared partially (i.e., up to a point of
+	 * dts_options[DTRACEOPT_STRSIZE].
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	int err;
+	const char *str1 = "foooooooooooooooo";
+	const char *str2 = "foooobaaaaaaaaaar";
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	state->dts_options[DTRACEOPT_STRSIZE] = 5;
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_regs[1] = (uintptr_t) str1;
+	estate->dtes_regs[2] = (uintptr_t) str2;
+
+	instr = DIF_INSTR_FMT(DIF_OP_SCMP, 1, 2, 3);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0, estate->dtes_cc_r);
+	ATF_CHECK_EQ(0, estate->dtes_cc_n);
+	ATF_CHECK_EQ(1, estate->dtes_cc_z);
+	ATF_CHECK_EQ(0, estate->dtes_cc_c);
+	ATF_CHECK_EQ(0, estate->dtes_cc_v);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
 #endif
 
 ATF_TP_ADD_TCS(tp)
@@ -2990,6 +3035,7 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, DIF_OP_SCMP_EQ);
 	ATF_TP_ADD_TC(tp, DIF_OP_SCMP_STR1_GT_STR2);
 	ATF_TP_ADD_TC(tp, DIF_OP_SCMP_STR1_LT_STR2);
+	ATF_TP_ADD_TC(tp, DIF_OP_SCMP_FAIL);
 #endif
 
 	return (atf_no_error());
