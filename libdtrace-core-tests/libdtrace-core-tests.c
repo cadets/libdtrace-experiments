0caa62541132cce82c5a3ea490e881fb8dbaaafc
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-19 11:20:33 +0100

    Test access to the ERRNO variable.

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -4056,6 +4056,45 @@ ATF_TC_BODY(DIF_VAR_GID, tc)
 	free(estate);
 }
 
+ATF_TC_WITHOUT_HEAD(DIF_VAR_ERRNO);
+ATF_TC_BODY(DIF_VAR_ERRNO, tc)
+{
+	/*
+	 * Test the ERRNO variable access.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	dtrace_id_t probeid;
+	dtrace_provider_id_t id;
+	dtrace_provider_t *provider;
+	int err;
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	state->dts_cred.dcr_action |= DTRACE_CRA_PROC;
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_regs[2] = 0;
+	estate->dtes_regs[3] = 0;
+
+	instr = DIF_INSTR_LDV(DIF_OP_LDGS, DIF_VAR_ERRNO, 3);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(errno, estate->dtes_regs[3]);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
 #endif
 
 ATF_TP_ADD_TCS(tp)
@@ -4173,6 +4212,7 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, DIF_VAR_ZONENAME);
 	ATF_TP_ADD_TC(tp, DIF_VAR_UID);
 	ATF_TP_ADD_TC(tp, DIF_VAR_GID);
+	ATF_TP_ADD_TC(tp, DIF_VAR_ERRNO);
 #endif
 
 	return (atf_no_error());
