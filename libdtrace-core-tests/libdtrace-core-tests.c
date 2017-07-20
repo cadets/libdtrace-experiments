01e88db755ef79208a8e4b6f8a811851b571ef96
Domagoj Stolfa <ds815@cam.ac.uk>
2017-07-20 14:27:34 +0100

    Create a couple of tests for edge cases in strlen(), fix a comment in strchr().

--- libdtrace-core-tests/libdtrace-core-tests.c
+++ libdtrace-core-tests/libdtrace-core-tests.c
@@ -4340,13 +4340,115 @@ ATF_TC_BODY(DIF_SUBR_STRLEN_TOO_LONG, tc)
 	free(estate);
 }
 
+ATF_TC_WITHOUT_HEAD(DIF_SUBR_STRLEN_NEGATIVE_LENGTH);
+ATF_TC_BODY(DIF_SUBR_STRLEN_NEGATIVE_LENGTH, tc)
+{
+	/*
+	 * Test the strlen() subroutine given a regular string and negative
+	 * size.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	dtrace_id_t probeid;
+	dtrace_provider_id_t id;
+	dtrace_provider_t *provider;
+	int err;
+	char *string = "hello";
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	state->dts_options[DTRACEOPT_STRSIZE] = -100;
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_regs[2] = 100;
+	estate->dtes_regs[3] = (uint64_t) string;
+	mstate->dtms_access |= DTRACE_ACCESS_KERNEL;
+
+	instr = DIF_INSTR_PUSHTS(DIF_OP_PUSHTR, DIF_TYPE_STRING, 2, 3);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(1, estate->dtes_ttop);
+
+	instr = DIF_INSTR_CALL(DIF_SUBR_STRLEN, 3);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(5, estate->dtes_regs[3]);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
+ATF_TC_WITHOUT_HEAD(DIF_SUBR_STRLEN_GARBAGE_PTR);
+ATF_TC_BODY(DIF_SUBR_STRLEN_GARBAGE_PTR, tc)
+{
+	/*
+	 * Test strlen() by giving a garbage pointer. Breaks in userspace, is
+	 * fine in the kernel.
+	 */
+	dtrace_mstate_t *mstate;
+	dtrace_vstate_t *vstate;
+	dtrace_state_t *state;
+	dtrace_estate_t *estate;
+	dif_instr_t instr;
+	dtrace_id_t probeid;
+	dtrace_provider_id_t id;
+	dtrace_provider_t *provider;
+	int err;
+	char *string = "hello";
+
+	mstate = calloc(1, sizeof (dtrace_mstate_t));
+	vstate = calloc(1, sizeof (dtrace_vstate_t));
+	state = calloc(1, sizeof (dtrace_state_t));
+	estate = calloc(1, sizeof (dtrace_estate_t));
+
+	state->dts_options[DTRACEOPT_STRSIZE] = 100;
+
+	estate->dtes_regs[DIF_REG_R0] = 0;
+	estate->dtes_regs[2] = 100;
+	estate->dtes_regs[3] = (uint64_t) 123;
+	mstate->dtms_access |= DTRACE_ACCESS_KERNEL;
+
+	/*
+	 * This does not happen in the kernel due to the toxic range mechanism
+	 * that DTrace implements, however, in userspace, it's not as obvious as
+	 * to how to do that.
+	 */
+	atf_tc_expect_death("Accessing garbage");
+	instr = DIF_INSTR_PUSHTS(DIF_OP_PUSHTR, DIF_TYPE_STRING, 2, 3);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(1, estate->dtes_ttop);
+
+	instr = DIF_INSTR_CALL(DIF_SUBR_STRLEN, 3);
+	err = dtrace_emul_instruction(instr, estate, mstate, vstate, state);
+
+	atf_tc_expect_fail("Garbage pointer sent in: %zu", estate->dtes_regs[3]);
+
+	ATF_CHECK_EQ(0, err);
+	ATF_CHECK_EQ(0, estate->dtes_regs[3]);
+
+	free(mstate);
+	free(vstate);
+	free(state);
+	free(estate);
+}
+
 ATF_TC_WITHOUT_HEAD(DIF_SUBR_STRCHR_EXPECTED);
 ATF_TC_BODY(DIF_SUBR_STRCHR_EXPECTED, tc)
 {
 	/*
-	 * Test the strlen() subroutine given a string that's too long.
-	 *
-	 * Expected output is the DTRACEOPT_STRSIZE boundary.
+	 * Test the strchr() subroutine given expected input.
 	 */
 	dtrace_mstate_t *mstate;
 	dtrace_vstate_t *vstate;
@@ -4519,6 +4621,8 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, DIF_SUBR_STRLEN_NULL);
 	ATF_TP_ADD_TC(tp, DIF_SUBR_STRLEN_NOT_TERMINATED);
 	ATF_TP_ADD_TC(tp, DIF_SUBR_STRLEN_TOO_LONG);
+	ATF_TP_ADD_TC(tp, DIF_SUBR_STRLEN_NEGATIVE_LENGTH);
+	ATF_TP_ADD_TC(tp, DIF_SUBR_STRLEN_GARBAGE_PTR);
 	ATF_TP_ADD_TC(tp, DIF_SUBR_STRCHR_EXPECTED);
 #endif
 
