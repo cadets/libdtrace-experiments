6b170fe2ee0f4b618da0d965889e4f40deac1cfd
Domagoj Stolfa <ds815@cam.ac.uk>
2017-08-07 15:05:59 +0100

    Follow convention inside DTrace to set the function as extern.

--- libdtrace-core/dtrace_impl.h
+++ libdtrace-core/dtrace_impl.h
@@ -1296,10 +1296,10 @@ extern void dtrace_copystr(uintptr_t, uintptr_t, size_t, volatile uint16_t *);
 #endif
 
 #ifdef _DTRACE_TESTS
-int dtrace_emul_instruction(dif_instr_t, dtrace_estate_t *, dtrace_mstate_t *,
+extern int dtrace_emul_instruction(dif_instr_t, dtrace_estate_t *, dtrace_mstate_t *,
     dtrace_vstate_t *, dtrace_state_t *);
-dtrace_probe_t *dtrace_getprobe(dtrace_id_t);
-int dtrace_difo_validate(dtrace_difo_t *, dtrace_vstate_t *, uint_t, cred_t *);
+extern dtrace_probe_t *dtrace_getprobe(dtrace_id_t);
+extern int dtrace_difo_validate(dtrace_difo_t *, dtrace_vstate_t *, uint_t, cred_t *);
 #endif
 
 
