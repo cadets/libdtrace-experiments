773fc826dc99acd531948d97f725c5468b5d897d
Domagoj Stolfa <ds815@cam.ac.uk>
2017-08-29 12:25:35 +0100

    Add the first part of branching tests.

---
+++ libdtrace-core-tests/dif_op_bge.c
@@ -0,0 +1,75 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../libdtrace-core/dtrace.h"
+#include "../libdtrace-core/dtrace_impl.h"
+#include "../test-api/dtrace_api.h"
+
+#include "dtcheck.h"
+
+int
+main(void)
+{
+	/*
+	 * Test the BGE operation of the DTrace machine.
+	 */
+	dtapi_conf_t *dtapi_conf;
+	uint_t pc;
+	int err;
+
+	dtapi_conf = dtapi_init(100, 20, DTRACE_ACCESS_KERNEL);
+	dtapi_op_cmp(dtapi_conf, 7, 8, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 1234, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 0, ("pc (%u) != 0\n", pc));
+
+	dtapi_op_cmp(dtapi_conf, 8, 7, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 1234, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 1234, ("pc (%u) != 1234\n", pc));
+
+	dtapi_op_cmp(dtapi_conf, 7, 7, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 0, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 0, ("pc (%u) != 0\n", pc));
+
+	dtapi_op_cmp(dtapi_conf, -7, 7, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 1234, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 0, ("pc (%u) != 0\n", pc));
+
+	dtapi_op_cmp(dtapi_conf, 7, -7, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 1234, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 1234, ("pc (%u) != 1234\n", pc));
+
+	dtapi_op_cmp(dtapi_conf, -7, -7, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 0, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 0, ("pc (%u) != 0\n", pc));
+
+	dtapi_op_cmp(dtapi_conf, -7, -10, &err);
+	DTCHECK(err, ("CMP failed: %s\n", strerror(err)));
+
+	pc = dtapi_op_bge(dtapi_conf, 1234, &err);
+	DTCHECK(err, ("BGE failed: %s\n", strerror(err)));
+	DTCHECK(pc != 1234, ("pc (%u) != 1234\n", pc));
+
+	dtapi_deinit(dtapi_conf);
+	return (0);
+}
+
