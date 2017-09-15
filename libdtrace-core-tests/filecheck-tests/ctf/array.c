6d16b6d521fe03f4e70aff02b4abf86ebf6ae954
Domagoj Stolfa <ds815@cam.ac.uk>
2017-09-15 14:52:16 +0100

    Add a regression test for arrays from Robert Mustacchi.

---
+++ libdtrace-core-tests/filecheck-tests/ctf/array.c
@@ -0,0 +1,17 @@
+/*
+ * Imported from: https://github.com/rmustacc/ctf-tests
+ *
+ * Credit: Robert Mustacchi
+ */
+
+int a[3];
+double b[42];
+const char *c[] = { "17" "31", "169" };
+
+int d[4][5];
+int e[4][5][6];
+int f[4][5][6][7];
+int g[4][5][6][7][8];
+int h[4][5][6][7][8][9];
+int i[4][5][6][7][8][9][10];
+
